2e387d0c320a973742c4f40124fe5503a1d7cf53
xiaopeng11 <1056400612@qq.com>
2015-10-14 16:58:48 +0800

    test
    1.0.0

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GaoDeMap
+//
+//  Created by mac on 15/9/12.
+//  Copyright (c) 2015年 wxhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MAMapKit/MAMapKit.h>
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  GaoDeMap
+//
+//  Created by mac on 15/9/12.
+//  Copyright (c) 2015年 wxhl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    mamap
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
