db811ec8eaf3c8b05acfe24b230ee0994547587b
xiaopeng11 <1056400612@qq.com>
2015-10-14 17:08:37 +0800

    1.0.2

--- ViewController.m
+++ ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    mamap
+    NSLog(@"asfadcf");
 }
 
 - (void)didReceiveMemoryWarning {
